9ea08ead9a8d1bc3a711a01df00aedfc9e021bf2
wnortier <warwick.nortier@gmail.com>
2023-03-19 12:36:48 +0200

    removed unused imports

--- __tests__/ts-paginator.test.ts
+++ __tests__/ts-paginator.test.ts
@@ -1,17 +1,16 @@
 import useTsPaginator from '../ts-paginator';
-import { renderHook, waitFor } from '@testing-library/react';
-import React from 'react';
+import { renderHook } from '@testing-library/react';
 describe('ts-paginator', () => {
   const { result } = renderHook(() => useTsPaginator(20, 10, 0));
   const {
     totalRecordCount,
     rowsPerPage,
     currentPage,
-    determinePaginationMessage,
-    determinePaginationDisabledState,
-    determineRowsPerPageOptions,
-    handleChangePage,
-    handleChangeRowsPerPage,
+    _determinePaginationMessage,
+    _determinePaginationDisabledState,
+    _determineRowsPerPageOptions,
+    _handleChangePage,
+    _handleChangeRowsPerPage,
   } = result.current;
 
   test('It should initialize correctly', () => {
@@ -20,12 +19,12 @@ describe('ts-paginator', () => {
     expect(currentPage).toBe(0);
   });
   test('It should calc the pagination message', () => {
-    expect(determinePaginationMessage()).toBe('Displaying 1 to 10 of 20 records');
+    expect(_determinePaginationMessage()).toBe('Displaying 1 to 10 of 20 records');
   });
   test('It should calc the options for the rows per page', () => {
-    expect(determineRowsPerPageOptions()).toEqual([10, 25, 50]);
+    expect(_determineRowsPerPageOptions()).toEqual([10, 25, 50]);
   });
   test('It should calc the disabled state', () => {
-    expect(determinePaginationDisabledState()).toBe(false);
+    expect(_determinePaginationDisabledState()).toBe(false);
   });
 });
